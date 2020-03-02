3b9a15c6001feebe757e06645f20b9687bad18fa
Dimas Agung Yudono <dimasday90@gmail.com>
2020-03-02 18:37:02 +0700

    React + 3rd API progress: using JokesAPI, create app with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import JokeList from './JokeList'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const categoryOptions = ['Any', 'Programming', 'Miscellaneous', 'Dark']
+const typeOptions = [{
+  value: 'single',
+  label: 'Single'
+}, {
+  value: 'twopart',
+  label: 'Two Part'
+}]
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      category: 'Any',
+      type: 'single',
+      count: 1,
+      showJokeList: false,
+      jokeList: [],
+      loading: false
+    }
+    this.inputHandleChange = this.inputHandleChange.bind(this)
+    this.submitJoke = this.submitJoke.bind(this)
+  }
+
+  inputHandleChange (event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  componentDidMount () {
+    document.title = 'JokeReactJS'
+  }
+
+  submitJoke (event) {
+    event.preventDefault()
+    this.setState({
+      loading: true
+    }, () => {
+      let jokes = []
+      for(let i = 0; i < this.state.count; i++) {
+        jokes.push(axios.get(`https://sv443.net/jokeapi/v2/joke/${this.state.category}?type=${this.state.type}`))
+      }
+      Promise.all(jokes) 
+        .then(result => {
+          this.setState({
+            showJokeList: true,
+            jokeList: result,
+            loading: false
+          })
+        })
+        .catch(err => {
+          this.setState({
+            loading: false
+          })
+        })
+    })
+  }
+
+  render () {
+    return (
+      <>
+        <header>
+          <h1>Welcome to JokeReactJS</h1>
+        </header>
+        <div id="container">
+          <form id="joke-form" onSubmit={this.submitJoke}>
+            <TextField
+              className="input-field"
+              name="category"
+              select
+              label="Select Category"
+              value={this.state.category}
+              onChange={this.inputHandleChange}
+            >
+              {categoryOptions.map(options => (
+                <MenuItem key={options} value={options}>{options}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              className="input-field"
+              name="type"
+              select
+              label="Select Joke Type"
+              value={this.state.type}
+              onChange={this.inputHandleChange}
+            >
+              {typeOptions.map(options => (
+                <MenuItem key={options.value} value={options.value}>{options.label}</MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              error={this.state.count > 10 || this.state.count < 1}
+              className="input-field"
+              name="count"
+              type="number"
+              label="Count Number"
+              min="1"
+              max="10"
+              scale="1"
+              value={this.state.count}
+              onChange={this.inputHandleChange}
+              helperText="Input count number between 1 to 10"
+            />
+            <div className="wrapper">
+              <Button 
+                type="submit" 
+                className="submit-btn" 
+                variant="outlined" 
+                color="primary"
+                disabled={this.state.loading}
+              >Find Your Joke
+              </Button>
+              {this.state.loading && <CircularProgress size={24} className="buttonProgress" />}
+            </div>
+          </form>
+        </div>
+        <JokeList show={this.state.showJokeList} category={this.state.category} type={this.state.type} jokes={this.state.jokeList} />
+      </>
+    )
+  }
 }
-
-export default App;
---
+++ src/JokeList.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+export default class Joke extends React.Component {
+    render () {
+        if (this.props.show) {
+            if (this.props.type === 'twopart') {
+                return (
+                    <div
+                        id="joke-list"
+                    >
+                        <header>
+                            <h3>{this.props.type} type of {this.props.category} joke(s)</h3>
+                        </header>
+                        {this.props.jokes.map(joke => (
+                            <Card className="joke-card" variant="outlined">
+                                <CardContent>
+                                    <Typography
+                                        variant="h5"
+                                        component="h2"
+                                    >
+                                        {joke.data.setup}
+                                    </Typography>
+                                    <Typography
+                                        variant="body"
+                                        component="p"
+                                    >
+                                        {joke.data.delivery}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+                )
+            } else {
+                return (
+                    <div
+                        id="joke-list"
+                    >
+                        <header>
+                            <h3>{this.props.type} type of {this.props.category} joke(s)</h3>
+                        </header>
+                        {this.props.jokes.map(joke => (
+                            <Card className="joke-card" variant="outlined">
+                                <CardContent>
+                                    <Typography
+                                        variant="h5"
+                                        component="h2"
+                                    >
+                                        {joke.data.joke}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+                )
+            }
+        } else {
+            return (
+                <>
+                </>
+            )
+        }
+    }
+}
